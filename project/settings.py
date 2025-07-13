85b82b19b2d46c2bc9e37d3728ec6ad1a517bdf7
Digambar Singh <singhdigambar266@gmail.com>
2025-07-13 11:57:21 +0530

    Fix: css for Render

--- project/settings.py
+++ project/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'music/static')] 
+STATICFILES_DIRS = [BASE_DIR / 'music' / 'static']
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = '/auth/login/'
 MEDIA_URL = '/media/'
